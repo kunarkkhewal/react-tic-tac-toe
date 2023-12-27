95cd1036a2556110883b452080befb52c1d38c20
kunarkkhewal <kunark.khewal88@gmail.com>
2023-12-27 18:46:26 +0530

    feat: added condition for winner

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,51 @@ function App() {
   const [matrixValues, setMatrixValues] = useState<MatrixValues>(() =>
     Array.from({ length: 3 }, () => Array(3).fill(""))
   );
+  const [winner, setWinner] = useState<string>('');
 
   const handleSquareClick = (num1: number, num2: number): void => {
-    if (matrixValues[num1][num2]) { 
+    if (winner || matrixValues[num1][num2]) { 
       return;
     }
     const updatedMatrix: MatrixValues = [...matrixValues];
     updatedMatrix[num1][num2] = isXTurn ? 'X' : 'O';
     setMatrixValues(updatedMatrix);
     setIsXTurn(!isXTurn);
+
+    const winnerStatus = getWinningStatus();
+    if (winnerStatus) {
+      setWinner(winnerStatus);
+      return;
+    }
   };
+
+  const getWinningStatus = () => {
+    const conditions = [
+      [[0,0], [0,1], [0,2]], 
+      [[1,0], [1,1], [1,2]], 
+      [[2,0], [2,1], [2,2]], 
+      [[0,0], [1,0], [2,0]], 
+      [[0,1], [1,1], [2,1]], 
+      [[0,2], [1,2], [2,2]], 
+      [[0,0], [1,1], [2,2]], 
+      [[0,2], [1,1], [2,0]]
+    ]
+    for(let index = 0; index < conditions.length; index++) {
+      const [a, b, c] = conditions[index];
+      if (matrixValues[a[0]][a[1]] && matrixValues[a[0]][a[1]] === matrixValues[b[0]][b[1]] && matrixValues[a[0]][a[1]] === matrixValues[c[0]][c[1]]) {
+        return matrixValues[a[0]][a[1]];
+      }
+    }
+    return null;
+  }
 
   return (
     <>
       <p className="text-xl font-bold text-red-700 text-center">
         Hello Duniya, welcome to TIC-TAC-TOE
+      </p>
+      <p className="text-xl font-bold text-red-700 text-center">
+        {winner ? `winner is ${winner}` : ''}
       </p>
       <div className="grid grid-rows-3 grid-cols-3 gap-0 w-36 mx-auto">
         {[0, 1, 2].map((row) =>
